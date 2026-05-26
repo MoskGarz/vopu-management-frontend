64df5e93c4fe07723657549fb926c989266937ba
TheChocolistoGuy <Felipemoskg@gmail.com>
2026-05-26 10:00:30 -0500

    fix: se agregaron los textos faltantes para traducir en el front, se agregó un DNS para que el frontend se pudiera conectar directamente al tolgee para resolver las claves de los mensajes

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="flex items-center justify-center h-full p-8">
-    <p class="text-[#3e4850]">Bienvenido a Vopu Management</p>
+    <p class="text-[#3e4850]">{{ t('dashboard.bienvenida') }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
